559b69ef8b80f92a975ca6c3898cb2b2bfa885af
AbdulahiAwil <aamosman10@gmail.com>
2025-04-06 21:10:11 +0300

    Add Profile page design

--- F_S_Supabace/src/Components/Header.jsx
+++ F_S_Supabace/src/Components/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
                 {isLoggedIn ? (
                     <>
                     <div> 
-                    <span className='text-gray-700 text-sm'>Hello, {profile.username}</span>
+                    <span className='text-gray-700 text-sm'>Hello, {profile?.username}</span>
                     </div>
                     <div className='relative'>
                         <button className='flex items-center justify-center w-8 h-8 rounded-full bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500' 
@@ -65,7 +65,7 @@ const Header = () => {
                              onMouseLeave={()=>setIsDropdownOpen(false)}
                              >
                              <div></div>
-                             <Link className='block px-4 py-2 text-gray-700 hover:bg-gray-100'>Your Profile</Link>
+                             <Link to={'profile'} className='block px-4 py-2 text-gray-700 hover:bg-gray-100'>Your Profile</Link>
                              <Link className='block px-4 py-2 text-gray-700 hover:bg-gray-100' >Manage Articles</Link>
                              <button onClick={()=> logout()} className='block px-4 py-2 text-gray-700 hover:bg-gray-100' >Signout</button>
                          </div>
